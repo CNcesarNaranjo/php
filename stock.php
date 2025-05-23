a75c11435ad622b83cc84c90d62eaed6cf3fb260
César Naranjo <cesnaranjo1089@gmail.com>
2025-05-23 03:04:53 -0500

    cambios

---
+++ stock.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="s">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stock</title>
+</head>
+<body>
+    <?php
+    $stock = 800;
+    if ($stock >0) {
+        echo "Hay Stock";
+    } else 
+    echo "No hay Stock";
+
+
+    ?>
+</body>
+</html>
