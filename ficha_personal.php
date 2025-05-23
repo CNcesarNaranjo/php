ed09bc614e541618c5f1138ce606c7ae8df3f4b2
César Naranjo <cesnaranjo1089@gmail.com>
2025-05-23 02:33:39 -0500

    cambios

---
+++ ficha_personal.php
@@ -0,0 +1,63 @@
+<?php
+
+date_default_timezone_set("America/Bogota");
+
+$nombre = "César David Parra";
+$edad = "35";
+$aPeliculas = array("Volver al futuro", "Titanic", "La Momia");
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <title>Ficha personal</title>
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1>Ficha Personal</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <tr>
+                        <th>Fecha:</th>
+                        <td><?php echo date("d/m/Y"); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Nombre y Apellidos:</th>
+                        <td><?php echo $nombre ?> </td>
+                    </tr>
+                    <tr>
+                        <th>Edad:</th>
+                        <td><?php echo $edad ?></td>
+                    </tr>
+                    <tr>
+                        <th>Peliculas favoritas:</th>
+                        <td><?php echo $aPeliculas[0]; ?><br>
+                            <?php echo $aPeliculas[1]; ?><br>
+                            <?php echo $aPeliculas[2]; ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </main>
+
+
+
+</body>
+
+</html>
